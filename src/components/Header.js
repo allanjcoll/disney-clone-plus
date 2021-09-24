46744c27f187c14f04e850a672a5d501c7386b47
allanjcoll <allancollantes@gmail.com>
2021-09-24 14:53:31 +0800

    setup2

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Styled from 'styled-components'
+
+function Header() {
+    return (
+       <Container>
+           Hello
+       </Container>
+    )
+}
+
+export default Header
+
+const Container = Styled.div`
+
+`
